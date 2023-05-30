c21a435c8acfc7f2455092b401d4523ba341bffe
lincxx <bossc@BOSSCO>
2023-05-30 08:55:42 -0400

    Add Item and Category views

--- puddle/item/models.py
+++ puddle/item/models.py
@@ -15,6 +15,9 @@ class Category(models.Model):
 class Item(models.Model):
     Category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
+    brewery = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=255, blank=True, null=True)
+    state = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     abv = models.CharField(max_length=10, blank=True, null=True)
     ibu = models.CharField(max_length=10, blank=True, null=True)
@@ -23,3 +26,6 @@ class Item(models.Model):
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
