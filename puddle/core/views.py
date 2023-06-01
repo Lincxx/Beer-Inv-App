159f9dd6a9b4ae247ad2bfd1864040a2cdac7568
lincxx <bossc@BOSSCO>
2023-06-01 09:02:15 -0400

    Add signup and login

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from item.models import Category, Item
+
+from .forms import SignupForm
+
 
 def index(request):
     # get the 6 newest items
@@ -8,9 +11,27 @@ def index(request):
     categories = Category.objects.all()
 
     return render(request, 'core/index.html', {
-       'categories': categories, 
-       'items': items
+        'categories': categories,
+        'items': items
     })
+
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+
+def signup(request):
+    # checking to see if form as been submitted
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
