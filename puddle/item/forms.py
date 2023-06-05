a408f94000b30c66de56847a0914065b9bfd41c8
lincxx <bossc@BOSSCO>
2023-06-05 06:59:37 -0400

    Add deletel and edit

--- puddle/item/forms.py
+++ puddle/item/forms.py
@@ -2,9 +2,82 @@ from django import forms
 
 from .models import Item
 
+INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
+
 
 class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ('category', 'name', 'brewery', 'city', 'state',
                   'description', 'abv', 'ibu', 'image', 'quantity')
+        widgets = {
+            'category': forms.Select(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'brewery': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'city': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'state': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'abv': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'ibu': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'quantity': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            })
+        }
+
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('category', 'name', 'brewery', 'city', 'state',
+                  'description', 'abv', 'ibu', 'image', 'quantity')
+        widgets = {
+            'category': forms.Select(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'brewery': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'city': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'state': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'abv': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'ibu': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'quantity': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            })
+        }
--- puddle/item/urls.py
+++ puddle/item/urls.py
@@ -8,4 +8,7 @@ app_name = 'item'
 urlpatterns = [
     path('new/', views.new, name='new'),
     path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/delete/', views.delete_item, name='delete'),
+    path('<int:pk>/edit/', views.edit, name='edit'),
+
 ]
